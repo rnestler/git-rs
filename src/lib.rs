82a65318b363b8b4927f28537405c87487397895
Raphael Nestler <raphael.nestler@gmail.com>
2016-10-28 14:21:59 +0200

    Add support for parsing index extensions

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 #[macro_use]
 extern crate nom;
 
-use nom::{IResult, be_u32};
+use nom::{IResult, be_u32, length_value};
 
 pub struct IndexEntry {
     pub ctime_s: u32,
@@ -18,7 +18,23 @@ pub struct IndexEntry {
     pub flags: u16,
 }
 
-pub struct IndexExtension {
+pub struct IndexExtension<'a> {
+    /// Signature starts with 'A'-'Z'
+    pub signature: &'a [u8],
+    pub data: &'a [u8],
+}
+
+pub fn parse_index_extension(input: &[u8]) -> IResult<&[u8], IndexExtension> {
+    chain!(input,
+           signature: take!(4) ~
+           data: length_value,
+           || {
+               IndexExtension{
+                   signature: signature,
+                   data: data,
+               }
+           }
+          )
 }
 
 /// The index header consists of 12 bytes:
@@ -47,10 +63,10 @@ pub fn parse_header(input: &[u8]) -> IResult<&[u8], IndexHeader> {
           )
 }
 
-pub struct Index {
+pub struct Index<'a> {
     pub header: IndexHeader,
     pub entries: Vec<IndexEntry>,
-    pub extensions: Vec<IndexExtension>,
+    pub extensions: Vec<IndexExtension<'a>>,
     pub sha1_checksum: [u8; 20],
 }
 
@@ -63,5 +79,14 @@ mod tests {
         let header = parse_header(d).unwrap().1;
         assert_eq!(header.version, 2);
         assert_eq!(header.index_entries, 1);
+    }
+
+    #[test]
+    fn test_parse_index_extension() {
+        let d: &[u8] = b"ABCD\x01\x02";
+        let extension = parse_index_extension(d).unwrap().1;
+
+        assert_eq!(extension.signature, b"ABCD");
+        assert_eq!(extension.data, &[2]);
     }
 }
