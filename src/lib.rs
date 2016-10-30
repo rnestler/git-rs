cbab99a42ea7c3fa4a25a5ca9146cabd23617c17
Raphael Nestler <raphael.nestler@gmail.com>
2016-10-30 23:15:37 +0100

    WIP working version :)

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 #[macro_use]
 extern crate nom;
 
-use nom::{IResult, be_u16, be_u32, length_value};
+use nom::{IResult, be_u16, be_u32};
 
 #[derive(Debug)]
 pub struct IndexEntry<'a> {
@@ -18,6 +18,7 @@ pub struct IndexEntry<'a> {
     /// 20 bytes SHA1 sum
     pub sha_1: &'a [u8],
     pub flags: u16,
+    pub path: &'a str,
 }
 
 pub fn parse_index_entry(input: &[u8]) -> IResult<&[u8], IndexEntry> {
@@ -33,7 +34,11 @@ pub fn parse_index_entry(input: &[u8]) -> IResult<&[u8], IndexEntry> {
            gid: be_u32 ~
            file_size: be_u32 ~
            sha_1: take!(20) ~
-           flags: be_u16,
+           flags: be_u16 ~
+           // TODO if path_lengh is bigger than 0xFFF this will fail
+           path: take_str!(flags & 0xFFF) ~
+           // TODO calculate padding
+           _padding: take!(2) ,
            || {
                IndexEntry {
                    ctime_s: ctime_s,
@@ -48,6 +53,7 @@ pub fn parse_index_entry(input: &[u8]) -> IResult<&[u8], IndexEntry> {
                    file_size: file_size,
                    sha_1: sha_1,
                    flags: flags,
+                   path: path,
                }
            }
     )
@@ -60,10 +66,28 @@ pub struct IndexExtension<'a> {
     pub data: &'a [u8],
 }
 
+/// Parse as many extensions as possible, but leave 20 bytes for the SHA-1 checksum in the input
+pub fn parse_index_extensions(input: &[u8]) -> IResult<&[u8], Vec<IndexExtension>> {
+    let head = &input[0..input.len()-20];
+    let tail = &input[input.len()-20..];
+
+    println!("head {:?}", head);
+    println!("tail {:?}", tail);
+
+    let extensions: IResult<&[u8], Vec<IndexExtension>> = many0!(head, parse_index_extension);
+    println!("{:?}", extensions);
+
+    // TODO get rid of this unwrap
+    let extensions = extensions.unwrap().1;
+
+    IResult::Done(tail, extensions)
+}
+
 pub fn parse_index_extension(input: &[u8]) -> IResult<&[u8], IndexExtension> {
     chain!(input,
            signature: take!(4) ~
-           data: length_value,
+           length: be_u32 ~
+           data: take!(length),
            || {
                IndexExtension{
                    signature: signature,
@@ -111,7 +135,7 @@ pub fn parse_index(input: &[u8]) -> IResult<&[u8], Index> {
     chain!(input,
            header: parse_header ~
            entries: count!(parse_index_entry, header.index_entries as usize) ~
-           extensions: many0!(parse_index_extension) ~
+           extensions: parse_index_extensions ~
            sha1_checksum: take!(20),
            || {
                Index {
@@ -137,11 +161,32 @@ mod tests {
 
     #[test]
     fn test_parse_index_extension() {
-        let d: &[u8] = b"ABCD\x01\x02";
+        let d: &[u8] = b"ABCD\x00\x00\x00\x01\x02";
         let extension = parse_index_extension(d).unwrap().1;
 
-        assert_eq!(extension.signature, b"ABCD");
-        assert_eq!(extension.data, &[2]);
+        assert_eq!(extension.signature, b"ABCD", "Wrong signature");
+        assert_eq!(extension.data, &[2], "Wrong data");
+    }
+
+    #[test]
+    fn test_parse_index_extensions() {
+        let mut d: [u8; 29] = [0; 29];
+
+        for (i, v) in b"ABCD\x00\x00\x00\x01\x02".iter().enumerate() {
+            d[i] = *v;
+        }
+
+        println!("input: {:?}", d);
+
+        let extensions = parse_index_extensions(&d).unwrap();
+
+        println!("output: {:?}", extensions);
+
+        assert_eq!(extensions.0.len(), 20);
+
+        assert_eq!(extensions.1.len(), 1);
+        assert_eq!(extensions.1[0].signature, b"ABCD");
+        assert_eq!(extensions.1[0].data, &[2]);
     }
 
     #[test]
